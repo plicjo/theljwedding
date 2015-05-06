c33362c370216191b776578eb6386457883476eb
Joshua Plicque <plicjo@gmail.com>
2015-05-06 16:25:01 -0400

    Rename static_pages_controller to admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'weddings/edit'
-
   devise_for :users
   devise_scope :user do
     get 'admin', to: 'devise/sessions#new'
     get '/admins/sign_out', to: 'devise/sessions#destroy'
   end
   
-  get 'static_pages/dashboard', as: :dashboard
+  get 'admin/dashboard', as: :dashboard
   root 'home#index'
 end

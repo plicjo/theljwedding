9b5444f2b3a65eaefefd8d7f5a5265c456fa54a1
Joshua Plicque <plicjo@gmail.com>
2015-05-06 14:42:54 -0400

    Remove updating wedding info functionality. Will not be used

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
     get 'admin', to: 'devise/sessions#new'
     get '/admins/sign_out', to: 'devise/sessions#destroy'
   end
-
-  resources :weddings, only: [:edit, :update]
   
   get 'static_pages/dashboard', as: :dashboard
   root 'home#index'

2987fc6698b194c981d6d4d4860dffb200f42a1c
Joshua Plicque <plicjo@gmail.com>
2015-05-30 13:04:45 -0400

    Restyle homepage -WIP

--- vendor/assets/javascripts/custom.prettyPhoto.js
+++ vendor/assets/javascripts/custom.prettyPhoto.js
@@ -28,7 +28,7 @@
       autoplay: true, /* Automatically start videos: True/False */
       modal: false, /* If set to true, only the close button will close the window */
       deeplinking: true, /* Allow prettyPhoto to update the url to enable deeplinking. */
-      overlay_gallery: true, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
+      overlay_gallery: false, /* If set to true, a gallery will overlay the fullscreen image on mouse over */
       overlay_gallery_max: 30, /* Maximum number of pictures in the overlay gallery */
       keyboard_shortcuts: true, /* Set to false if you open forms inside prettyPhoto */
       changepicturecallback: function(){}, /* Called everytime an item is shown/changed */

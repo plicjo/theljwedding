f0f41d22b4c7286ed89e2a0217e0cc4676f36c19
Hashrocket Workstation <dev@hashrocket.com>
2015-06-02 16:42:02 -0400

    Configure test suite

--- features/step_definitions/cookbook_steps.rb
+++ features/step_definitions/cookbook_steps.rb
@@ -3,32 +3,30 @@ Given(/^I am on the homepage$/) do
 end
 
 When(/^I fill out a valid recipe$/) do
-  VCR.use_cassette 'amazon_s3', allow_playback_repeats: true, record: :new_episodes do
-    attach_file 'recipe_photo', 'spec/support/photo.jpg'
-    fill_in 'recipe_title', with: 'Darkhorse Pie'
-    fill_in 'recipe_family_name', with: 'Olsen Family'
+  attach_file 'recipe_photo', 'spec/support/photo.jpg'
+  fill_in 'recipe_title', with: 'Darkhorse Pie'
+  fill_in 'recipe_family_name', with: 'Olsen Family'
 
-    click_link_or_button 'Add Ingredient'
-    expect(page).to have_content 'Remove Ingredient'
+  click_link_or_button 'Add Ingredient'
+  expect(page).to have_content 'Remove Ingredient'
 
-    within '#ingredients' do
-      find('.recipe_ingredients_ingredient_description input').set('2 Carrots')
-    end
-
-     click_link_or_button 'Add Step'
-     expect(page).to have_content 'Remove Step'
-
-     within '#recipe_steps' do
-       find('.recipe_recipe_steps_step_description input').set('Chop the Carrots. NAO!')
-     end
-
-    fill_in 'recipe_prep_time', with: '10 minutes'
-    fill_in 'recipe_cook_time', with: '30 minutes'
-    fill_in 'recipe_number_of_servings', with: '6'
-    fill_in 'recipe_description', with: 'Cut Up Carrots from the exotic land of Indonesia. What a great recipe for a new couple!'
-
-    click_button 'Submit Recipe'
+  within '#ingredients' do
+    find('.recipe_ingredients_ingredient_description input').set('2 Carrots')
   end
+
+   click_link_or_button 'Add Step'
+   expect(page).to have_content 'Remove Step'
+
+   within '#recipe_steps' do
+     find('.recipe_recipe_steps_step_description input').set('Chop the Carrots. NAO!')
+   end
+
+  fill_in 'recipe_prep_time', with: '10 minutes'
+  fill_in 'recipe_cook_time', with: '30 minutes'
+  fill_in 'recipe_number_of_servings', with: '6'
+  fill_in 'recipe_description', with: 'Cut Up Carrots from the exotic land of Indonesia. What a great recipe for a new couple!'
+
+  click_button 'Submit Recipe'
 end
 
 Then(/^I should see that the recipe has been created$/) do
--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'vcr_cassettes'
   c.hook_into :webmock
   c.ignore_localhost = true
+  c.ignore_hosts 'theljwedding-all.s3.amazonaws.com'
 end

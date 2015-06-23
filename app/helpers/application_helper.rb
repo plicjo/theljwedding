25dff54b4000a540df863190624ee12398d7eaed
Joshua Plicque <plicjo@gmail.com>
2015-06-23 11:15:09 -0400

    Update copy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
       ['Chicken Florentine En Croute', food_options[2]],
       ['Roasted Prime Rib of Beef',    food_options[0]],
       ['Mahi-Mahi with a Fresh Mango Salsa or Lemon Herb Sauce', food_options[1]],
-      ['Roasted Vegetables in Puff Pantry', food_options[3]]
+      ['Roasted Vegetables in Puff Pastry', food_options[3]]
     ]
 
   end

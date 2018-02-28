3de089d7a7afee02937d39e52aa6680bab80f7dd
Joshua Plicque <plicjo@gmail.com>
2018-02-28 15:32:44 -0500

    Revert "Remove test steps"
    This reverts commit 8620a324d0e7b98edf8c0d85a963618864ffc82e.

--- features/step_definitions/rsvp_steps.rb
+++ features/step_definitions/rsvp_steps.rb
@@ -2,6 +2,30 @@ Given(/^I visit the RSVP page$/) do
   visit rsvp_path
 end
 
+When(/^I fill in the RSVP information$/) do
+  within '#new_rsvp' do
+    fill_in 'rsvp_email',      with: 'admin@example.com'
+    fill_in 'rsvp_first_name', with: 'Example'
+    fill_in 'rsvp_last_name',  with: 'Guest'
+    choose 'Accepts your invite with pleasure'
+    choose 'Roasted Prime Rib of Beef'
+  end
+end
+
+When(/^I add an additional guest$/) do
+  click_link 'Add Additional Guest to Your Party'
+  page.should have_content 'Remove additional guest'
+end
+
+When(/^I fill in the additional guest's information$/) do
+  within '#additional-guests' do
+    find('.rsvp_additional_guests_first_name input').set('Example')
+    find('.rsvp_additional_guests_last_name input').set('Guest')
+    choose 'Declines with regret'
+    choose 'Mahi-Mahi with a Fresh Mango Salsa'
+  end
+end
+
 Given(/^an rsvp exists$/) do
   @rsvp = FactoryGirl.create(:rsvp)
 end

85e9303f2fdc9e4d4143c10c038bb2c40f098823
Joshua Plicque <plicjo@gmail.com>
2020-01-08 23:32:43 -0500

    Upgrade to Rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -1,104 +1,99 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200109021625) do
+ActiveRecord::Schema.define(version: 2020_01_09_042656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "additional_guests", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "food_option"
-    t.integer  "rsvp_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.boolean  "invite_status"
+  create_table "additional_guests", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "food_option"
+    t.integer "rsvp_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "invite_status"
+    t.index ["rsvp_id"], name: "index_additional_guests_on_rsvp_id"
   end
 
-  add_index "additional_guests", ["rsvp_id"], name: "index_additional_guests_on_rsvp_id", using: :btree
-
-  create_table "guests", force: :cascade do |t|
-    t.string   "email"
-    t.boolean  "rsvp_status", default: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-  end
-
-  create_table "ingredients", force: :cascade do |t|
-    t.string   "ingredient_description"
-    t.integer  "recipe_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id", using: :btree
-
-  create_table "recipe_steps", force: :cascade do |t|
-    t.string   "step_description"
-    t.integer  "recipe_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
-  add_index "recipe_steps", ["recipe_id"], name: "index_recipe_steps_on_recipe_id", using: :btree
-
-  create_table "recipes", force: :cascade do |t|
-    t.string   "title"
-    t.string   "family_name"
-    t.string   "prep_time"
-    t.string   "cook_time"
-    t.string   "number_of_servings"
-    t.text     "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-  end
-
-  create_table "rsvp_emails", force: :cascade do |t|
-    t.text     "body"
-    t.string   "subject"
+  create_table "guests", id: :serial, force: :cascade do |t|
+    t.string "email"
+    t.boolean "rsvp_status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "rsvps", force: :cascade do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "attending"
-    t.integer  "food_option"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.boolean  "invite_status"
+  create_table "ingredients", id: :serial, force: :cascade do |t|
+    t.string "ingredient_description"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "recipe_steps", id: :serial, force: :cascade do |t|
+    t.string "step_description"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_recipe_steps_on_recipe_id"
+  end
+
+  create_table "recipes", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.string "family_name"
+    t.string "prep_time"
+    t.string "cook_time"
+    t.string "number_of_servings"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rsvp_emails", id: :serial, force: :cascade do |t|
+    t.text "body"
+    t.string "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rsvps", id: :serial, force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "attending"
+    t.integer "food_option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "invite_status"
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipe_steps", "recipes"
